4fe88b0126c69bfe4f36cc0b750de27e52e44a0b
noo-dev <noo.dev23@gmail.com>
2024-03-01 15:19:15 +0500

    basic hash table implemented

--- hash_table/hast_table.js
+++ hash_table/hast_table.js
@@ -1,5 +1,27 @@
 class HashTable {
-  // write implementation
+  constructor() {
+    this.size = 1000
+    this.buckets = Array(this.size).fill(null); 
+  }
+
+  hash(key) {
+    let hash = 0;
+    for (const char of key) {
+      hash += char.charCodeAt(0)
+    }
+
+    return hash % this.size;
+  }
+
+  set(key, value) {
+    const keyHash = this.hash(key);
+    this.buckets[keyHash] = value;
+  }
+
+  get(key) {
+    const keyHash = this.hash(key);
+    return this.buckets[keyHash];
+  }
 }
 
 module.exports = HashTable;
